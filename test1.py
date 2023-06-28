41171a8ffc940ffa4d375bf207df12ed4967e00c
bang <057310igo@gmail.com>
2023-06-28 11:57:48 +0800

    modified 1

--- test1.py
+++ test1.py
@@ -1,9 +1,9 @@
 while True:
-        n=int(input())
-        if(n<=0):
+        a=int(input())
+        if(a<=0):
             break
         else:
-            for i in range(1,n+1):
-                if (n%i==0):
+            for i in range(1,a+1):
+                if (a%i==0):
                     print(i,end=" ")
         print("")
